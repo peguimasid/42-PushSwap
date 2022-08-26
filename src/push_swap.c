e7934e4df7be43dbdb103b8f6af213ad9855b33c
Guilhermo Masid <guilhermomasid@gmail.com>
2022-08-26 17:16:50 -0300

    chore: handle not send arguments and print just the numbers

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,21 +6,35 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 14:04:20 by gmasid            #+#    #+#             */
-/*   Updated: 2022/08/26 14:05:59 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/08/26 17:14:56 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int argc, char **argv)
+int	throw_error(char *error)
+{
+	ft_printf("\033[0;31m\n");
+	ft_printf("Error\n%s\n", error);
+	ft_printf("\033[0m");
+	return (1);
+}
+
+void	print_nums(char **args)
 {
 	int	i;
 
-	ft_printf("%d\n\n", argc);
 	i = 0;
-	while (argv[i])
+	while (args[i])
 	{
-		ft_printf("%s", argv[i]);
+		ft_printf("%s\n", args[i]);
 		i++;
 	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+		return (throw_error("Plase provide at least one number"));
+	print_nums(argv + 1);
 }
