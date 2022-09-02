8d1d7cd17e1bc8a847f0e75844bc56eec9ed08e7
Guilhermo Masid <guilhermomasid@gmail.com>
2022-09-02 20:05:19 -0300

    chore: create stack using index of sorting number

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 18:10:54 by gmasid            #+#    #+#             */
-/*   Updated: 2022/09/02 13:10:22 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/09/02 19:51:21 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,21 +51,116 @@ long	get_val(char *str)
 	return (result * sign);
 }
 
-int	fill_stack(t_stack *stack, int ac, char **av)
+int	contains_duplicates(int *array, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < size)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (array[i] == array[j])
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	fill_array(int *array, int argc, char **argv)
 {
 	int		i;
+	int		j;
 	long	val;
 
-	i = ac - 1;
-	while (i >= 0)
+	i = 0;
+	j = 0;
+	while (i < argc)
 	{
-		if (!is_num(av[i]))
+		if (!is_num(argv[i]))
 			return (0);
-		val = get_val(av[i]);
-		if (val == 2147483648 || includes(stack, val))
+		val = get_val(argv[i]);
+		if (val == 2147483648 || contains_duplicates(array, j))
 			return (0);
-		push(stack, val);
-		i--;
+		array[j] = get_val(argv[i]);
+		j++;
+		i++;
+	}
+	return (1);
+}
+
+int	*copy_array(int *array, int size)
+{
+	int	i;
+	int	*result;
+
+	i = 0;
+	result = malloc(sizeof(int) * size);
+	if (!result)
+		return (0);
+	while (i < size)
+	{
+		result[i] = array[i];
+		i++;
+	}
+	return (result);
+}
+
+int	sort_array(int *array, int size)
+{
+	int	i;
+	int	j;
+	int	temp;
+
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size - 1)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	fill_stack(t_stack *stack, int ac, char **av)
+{
+	int	i;
+	int	j;
+	int	*array;
+	int	*array_copy;
+
+	array = malloc(sizeof(int) * ac);
+	if (!fill_array(array, ac, av))
+		return (0);
+	array_copy = copy_array(array, ac);
+	sort_array(array_copy, ac);
+	i = 0;
+	while (i < ac)
+	{
+		j = 0;
+		while (j < ac)
+		{
+			if (array[i] == array_copy[j])
+			{
+				stack->array[i] = j;
+				break ;
+			}
+			j++;
+		}
+		i++;
 	}
 	return (1);
 }
--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 14:04:20 by gmasid            #+#    #+#             */
-/*   Updated: 2022/09/02 14:40:48 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/09/02 19:37:14 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ int	main(int argc, char **argv)
 {
 	t_stack	*stack_a;
 	t_stack	*stack_b;
-	int		val;
 
 	if (argc < 2)
 		return (throw_error("Plase provide at least one number"));
@@ -31,8 +30,7 @@ int	main(int argc, char **argv)
 	ft_printf("STACK B\n");
 	print_list(stack_b);
 	// ---
-	val = peek(stack_a, 1);
-	ft_printf("<<<%d>>>\n", val);
+	execute_operation("RA", stack_a, stack_b);
 	// ---
 	ft_printf("\n\n\n----------- AFTER ------------\n");
 	ft_printf("STACK A\n");
