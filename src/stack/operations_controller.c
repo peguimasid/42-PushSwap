457a8710907fd1505a7e589ef73a16a5ed0d77f5
Guilhermo Masid <guilhermomasid@gmail.com>
2022-09-03 16:44:13 -0300

    change operations to lowercase

--- src/stack/operations_controller.c
+++ src/stack/operations_controller.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 16:28:22 by gmasid            #+#    #+#             */
-/*   Updated: 2022/09/03 16:31:34 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/09/03 16:43:49 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,27 @@
 
 int	execute_operation(char *operation, t_stack *stack_a, t_stack *stack_b)
 {
-	if (ft_strncmp(operation, "SA", 2) == 0)
+	if (ft_strncmp(operation, "sa", 2) == 0)
 		return (swap(stack_a));
-	if (ft_strncmp(operation, "SB", 2) == 0)
+	if (ft_strncmp(operation, "sb", 2) == 0)
 		return (swap(stack_b));
-	if (ft_strncmp(operation, "SS", 2) == 0)
+	if (ft_strncmp(operation, "ss", 2) == 0)
 		return (swap(stack_a) && swap(stack_b));
-	if (ft_strncmp(operation, "RRA", 3) == 0)
+	if (ft_strncmp(operation, "rra", 3) == 0)
 		return (reverse_rotate(stack_a));
-	if (ft_strncmp(operation, "RRB", 3) == 0)
+	if (ft_strncmp(operation, "rrb", 3) == 0)
 		return (reverse_rotate(stack_b));
-	if (ft_strncmp(operation, "RRR", 3) == 0)
+	if (ft_strncmp(operation, "rrr", 3) == 0)
 		return (reverse_rotate(stack_a) && reverse_rotate(stack_b));
-	if (ft_strncmp(operation, "RA", 2) == 0)
+	if (ft_strncmp(operation, "ra", 2) == 0)
 		return (rotate(stack_a));
-	if (ft_strncmp(operation, "RB", 2) == 0)
+	if (ft_strncmp(operation, "rb", 2) == 0)
 		return (rotate(stack_b));
-	if (ft_strncmp(operation, "RR", 2) == 0)
+	if (ft_strncmp(operation, "rr", 2) == 0)
 		return (rotate(stack_a) && rotate(stack_b));
-	if (ft_strncmp(operation, "PA", 2) == 0)
+	if (ft_strncmp(operation, "pa", 2) == 0)
 		return (push_from(stack_b, stack_a));
-	if (ft_strncmp(operation, "PB", 2) == 0)
+	if (ft_strncmp(operation, "pb", 2) == 0)
 		return (push_from(stack_a, stack_b));
 	return (0);
 }
