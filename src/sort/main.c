19f3b66e8d862332c2ee3bf65ec683f7e7a3306e
Guilhermo Masid <guilhermomasid@gmail.com>
2022-09-06 20:24:32 -0300

    sort 3 element stack

--- src/sort/main.c
+++ src/sort/main.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 13:00:34 by gmasid            #+#    #+#             */
-/*   Updated: 2022/09/05 19:42:59 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/09/06 20:23:35 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,9 +49,51 @@ void	sort_big_stack(t_stack *stack_a, t_stack *stack_b)
 	}
 }
 
+// 0 2 1
+// 1 0 2
+// 1 2 0
+// 2 0 1
+// 2 1 0
+
+void	sort_3(t_stack *stack_a, t_stack *stack_b)
+{
+	int	top;
+	int	middle;
+	int	bottom;
+
+	top = peek(stack_a, 0);
+	middle = peek(stack_a, 1);
+	bottom = peek(stack_a, 2);
+	if (top > middle && bottom > middle && top < bottom)
+		dispatch_operation(SA, stack_a, stack_b);
+	if (top > middle && middle > bottom && top > bottom)
+	{
+		dispatch_operation(SA, stack_a, stack_b);
+		dispatch_operation(RRA, stack_a, stack_b);
+	}
+	if (top > middle && bottom > middle && top > bottom)
+		dispatch_operation(RA, stack_a, stack_b);
+	if (top < middle && middle > bottom && top < bottom)
+	{
+		dispatch_operation(SA, stack_a, stack_b);
+		dispatch_operation(RA, stack_a, stack_b);
+	}
+	if (top < middle && middle > bottom && top > bottom)
+		dispatch_operation(RRA, stack_a, stack_b);
+}
+
 void	solve(t_stack *stack_a, t_stack *stack_b)
 {
 	if (is_sorted(stack_a))
 		return ;
+	if (stack_a->size == 2)
+	{
+		dispatch_operation(SA, stack_a, stack_b);
+		return ;
+	}
+	if (stack_a->size == 3)
+		return (sort_3(stack_a, stack_b));
+	// if (stack_a->size <= 5)
+	// 	return (sort_5(stack_a, stack_b));
 	return (sort_big_stack(stack_a, stack_b));
 }
