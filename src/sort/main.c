7a73db9c526989498c356e78f5260167a50bf34d
Guilhermo Masid <guilhermomasid@gmail.com>
2022-09-07 14:12:40 -0300

    sort 5 element stack

--- src/sort/main.c
+++ src/sort/main.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 13:00:34 by gmasid            #+#    #+#             */
-/*   Updated: 2022/09/06 20:27:41 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/09/07 14:04:57 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,6 +76,30 @@ void	sort_3(t_stack *stack_a, t_stack *stack_b)
 		dispatch_operation(RRA, stack_a, stack_b);
 }
 
+void	sort_5(t_stack *stack_a, t_stack *stack_b)
+{
+	int	len;
+	int	i;
+	int	current_val;
+
+	len = stack_a->end_index;
+	i = 0;
+	while (i <= len && stack_b->end_index < 1)
+	{
+		current_val = peek(stack_a, 0);
+		if (current_val == 0 || current_val == 1)
+			dispatch_operation(PB, stack_a, stack_b);
+		else
+			dispatch_operation(RA, stack_a, stack_b);
+		i++;
+	}
+	sort_3(stack_a, stack_b);
+	if (is_sorted(stack_b))
+		dispatch_operation(SB, stack_a, stack_b);
+	while (stack_b->end_index >= 0)
+		dispatch_operation(PA, stack_a, stack_b);
+}
+
 void	solve(t_stack *stack_a, t_stack *stack_b)
 {
 	if (is_sorted(stack_a))
@@ -87,7 +111,7 @@ void	solve(t_stack *stack_a, t_stack *stack_b)
 	}
 	if (stack_a->size == 3)
 		return (sort_3(stack_a, stack_b));
-	// if (stack_a->size <= 5)
-	// 	return (sort_5(stack_a, stack_b));
+	if (stack_a->size <= 5)
+		return (sort_5(stack_a, stack_b));
 	return (sort_big_stack(stack_a, stack_b));
 }
