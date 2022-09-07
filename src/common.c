3c86c119b6708e2e97b472ba15f7756f9c5c245b
Guilhermo Masid <guilhermomasid@gmail.com>
2022-09-07 16:30:30 -0300

    remove print_list function

--- src/common.c
+++ src/common.c
@@ -6,7 +6,7 @@
 /*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 16:04:16 by gmasid            #+#    #+#             */
-/*   Updated: 2022/09/07 15:37:11 by gmasid           ###   ########.fr       */
+/*   Updated: 2022/09/07 16:30:23 by gmasid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,4 @@ int	throw_error(void)
 {
 	ft_putstr_fd("Error\n", STDERR_FILENO);
 	return (1);
-}
-
-// TODO: Remove this function
-void	print_list(t_stack *stack)
-{
-	int	i;
-
-	i = 0;
-	ft_printf("[");
-	while (i < stack->size)
-	{
-		ft_printf("%d", stack->array[i]);
-		if (i != stack->size - 1)
-			ft_printf(", ");
-		i++;
-	}
-	ft_printf("]");
 }
